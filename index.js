12620071b2f9596190229b6d8ef48702db6cf4c8
Hitesh Kumar <hitesh.kumar2187@gmail.com>
2023-02-22 17:55:04 +0530

    Added new console log, workflow should not trigger or increase version

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("HELLOW WORLD TO SEMANTIC");
 console.log("Hellow From another world");
 console.log("Minor version should change");
+console.log("Testing semantic release, this commit should not increase version");
